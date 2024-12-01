4c1208d8a9cc67b4b893e100f878d61ab71b0999
william thomas <william@blackboxco.com.au>
2024-12-01 16:39:32 +1100

    feat: added dockerfile and documentation

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,20 +44,22 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  host = System.get_env("PHX_HOST") || "localhost"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :pg_siphon_management, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :pg_siphon_management, PgSiphonManagementWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: host, port: port, scheme: "http"],
     http: [
-      # Enable IPv6 and bind on all interfaces.
-      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
-      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
-      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
       ip: {0, 0, 0, 0, 0, 0, 0, 0},
       port: port
+    ],
+    check_origin: [
+      "http://localhost:#{port}",
+      "http://127.0.0.1:#{port}",
+      "//localhost:#{port}",
+      "//127.0.0.1:#{port}"
     ],
     secret_key_base: secret_key_base
 
